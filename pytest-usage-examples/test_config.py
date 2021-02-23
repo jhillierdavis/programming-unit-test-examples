2a3d7c83d1a28f169020fb1bbdcdde23282b3b05
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-02-23 16:41:03 +0000

    Added missing configuration entry (option) & section unit tests.

--- pytest-usage-examples/test_config.py
+++ pytest-usage-examples/test_config.py
@@ -8,12 +8,32 @@ import configparser
 # database.user=bogus
 # database.password=Change1t!
 
-def test_read_config_entries():
-    # Given: configuration read from a properties file
+def _read_config_from_file():
     config = configparser.RawConfigParser()
     config.read('TestConfigFile.properties')
+    return config
+
+def test_read_valid_config_entries():
+    # Given: configuration read from a properties file
+    config = _read_config_from_file()
 
     # Then: configuration entry values are as expected
     assert("unitTest" == config.get('DatabaseSection', 'database.dbname'))
     assert("bogus" == config.get('DatabaseSection', 'database.user'))
     assert("Change1t!" == config.get('DatabaseSection', 'database.password'))
+
+def test_no_option_error():
+    # Given: configuration read from a properties file
+    config = _read_config_from_file()
+
+    # Then: missing configuration entry values raises an exception
+    with pytest.raises(configparser.NoOptionError):
+        config.get('DatabaseSection', 'database.missing.option')
+
+def test_no_section_error():
+    # Given: configuration read from a properties file
+    config = _read_config_from_file()
+
+    # Then: missing configuration section raises an exception
+    with pytest.raises(configparser.NoSectionError):
+        config.get('MissingSection', 'database.user')
