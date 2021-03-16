fc873d22416d003d8aa9eaec27a653e8bfeb29ad
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-03-16 23:34:30 +0000

    Added locale example (using currency).

---
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/locale/LocaleFormattingExamples.java
@@ -0,0 +1,41 @@
+package com.jhdit.java.learning.locale;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LocaleFormattingExamples {
+
+    @ParameterizedTest(name = "{index} => inputCurrency={0}, expectedResults={1}")
+    @CsvSource(value={
+            "UK|£27.89",
+            "US|$27.89",
+    },  delimiter = '|')
+    public void localeSpecificCurrency(String inputCurrency, String expectedResult)    {
+        // Setup: set locale (based on input)
+        Locale locale;
+        switch (inputCurrency) {
+            case "US":
+                locale = Locale.US;
+                break;
+
+            case "UK":
+                locale = Locale.UK;
+                break;
+
+            default:
+                locale = Locale.getDefault();
+        }
+
+        // When: currency formatter is initialised based on the specified locale
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(locale);
+
+        // Then: financial values are formatted in the appropriate currency (e.g. GBP or USD)
+        assertEquals(expectedResult, currencyFormat.format(27.89));
+    }
+}
