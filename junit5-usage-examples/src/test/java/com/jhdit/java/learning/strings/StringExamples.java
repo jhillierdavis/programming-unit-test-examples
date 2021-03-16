e7dbfcfca21476f04ff7d54246469cd4a7d9de6a
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-03-16 21:46:46 +0000

    Extended examples (of use of intern() method)

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/strings/StringExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/strings/StringExamples.java
@@ -2,8 +2,7 @@ package com.jhdit.java.learning.strings;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Strings are immutable in Java.
@@ -26,15 +25,20 @@ public class StringExamples {
 
     @Test
     public void stringInitialisationUsingDifferentStringPoolMemory() {
-        // Given: different string variable initialisation approaches using 'new'
+        // Given: different string variable initialisation approaches using 'new' (NOT recommended however typically)
         String x = "Hello";
         String y = new String("Hello");
         char[] text = {'H','e','l','l','o'};
         String z = new String(text);
 
         // Then: all point towards (reference) different objects in memory
+        assertFalse(x == y);
         assertNotSame(x,y);
         assertNotSame(x,z);
         assertNotSame(y,z);
+
+        // However: if use intern() then can obtain same (canonical) reference to "Hello" (from String pool)
+        String internStr = z.intern();
+        assertTrue(internStr == x);
     }
 }
