30e5363b90f4619fe9c8bc659e9abbabada0c42b
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-03-16 11:01:45 +0000

    Added more comment & examples.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/operators/OperatorPrecedenceTests.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/operators/OperatorPrecedenceTests.java
@@ -4,10 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Operator precedence & associativity rules
+ *
+ * (Precedence = priority, Associativity = order for same precedence)
+ *
+ * Parenthesis (brackets) can be used to override precedence & associativity
+ *
+ * Initial examples:
+ *
+ * 2 + 3 * 4 is interpreted as 2 + (3 * 4) , resulting in 14
+ * 1 + 2 - 3 is interpreted as ((1 + 2) - 3) since plus & minus have same precedence & left associativity
+ * - - 4 is interpreted as (- (-4) ) since the unary operator has right associativity
+ *
+ */
+
 public class OperatorPrecedenceTests {
 
     @Test
     public void multiplicativeBeforeAdditive() {
+        // Multiplication has precedence over addition
+        assertEquals(14, 2 + 3 * 4);
+        assertEquals(14, 2 + (3 * 4)); // Above is equivalent to this (using parenthesis)
+        assertEquals(20, (2 + 3) * 4);
+
         // Multiplication (& division) have precedence over addition (& subtraction)
         assertSame(7, 1 + 2 * 3);
         assertSame(7, 1 + (2 * 3));
