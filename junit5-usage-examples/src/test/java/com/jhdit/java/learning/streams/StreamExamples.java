c45144d0d884bda8d189f3a2d243ec796504ffd8
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-05 10:48:37 +0100

    Java stream examples (e.g. use of pipeline elements).

---
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/streams/StreamExamples.java
@@ -0,0 +1,44 @@
+package com.jhdit.java.learning.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.UnaryOperator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Product {
+    String name;
+
+    Product(String name)    {
+        this.name = name;
+    }
+
+    String getName()    {
+        return this.name;
+    }
+}
+
+public class StreamExamples {
+
+    private int sumNotWhiteSpaceChars(List<Product> products) {
+        Function<Product, String> nameMapper = p -> p.getName();
+        UnaryOperator<String> trimMapper = s -> s.trim();
+        ToIntFunction<String> lengthMapper = s -> s.length();
+        return products.stream().map(nameMapper.andThen(trimMapper)).mapToInt(lengthMapper).sum();
+    }
+
+    @Test
+    void exploreStreamPipelineElements()   {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Alpha"));
+        products.add(new Product("Beta"));
+        products.add(new Product("Gamma"));
+
+        // Then:
+        assertEquals(14, sumNotWhiteSpaceChars(products));
+    }
+}
