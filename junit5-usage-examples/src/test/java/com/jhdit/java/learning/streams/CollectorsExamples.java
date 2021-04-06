550e7c0a879c74da3b76a351a5342066357e26a3
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-06 10:18:50 +0100

    Removed used imports.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/streams/CollectorsExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/streams/CollectorsExamples.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
