760471d170912880a6b242f0df48614936c13836
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-06 09:50:33 +0100

    Added further examples.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/streams/CollectorsExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/streams/CollectorsExamples.java
@@ -15,15 +15,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CollectorsExamples {
 
-    private String concatenateViaStream(String[] values) {
+    private String concatenateValues(String[] values) {
         return Arrays.stream(values).collect( Collectors.mapping(s -> s.toString(), Collectors.joining(", ")));
     }
 
     @Test
-    void exploreReduceForCustomAggregation()    {
+    void exploreCollectorsMapping()    {
         // Given: an array of String
         String[] colours  = {"Red", "Yellow", "Green", "Blue"};
 
-        assertEquals("Red, Yellow, Green, Blue", concatenateViaStream(colours));
+        assertEquals("Red, Yellow, Green, Blue", concatenateValues(colours));
+    }
+
+    private long countResults(String[] values)    {
+        return Arrays.stream(values).collect( Collectors.summarizingInt( s -> s.length() ) ).getCount();
+    }
+
+    private long totalLength(String[] values)    {
+        return Arrays.stream(values).collect( Collectors.summarizingInt( s -> s.length() ) ).getSum();
+    }
+
+    private int maxLength(String[] values) {
+        return Arrays.stream(values).collect( Collectors.summarizingInt( s -> s.length() ) ).getMax();
+    }
+
+    private int minLength(String[] values) {
+        return Arrays.stream(values).collect( Collectors.summarizingInt( s -> s.length() ) ).getMin();
+    }
+
+    private double meanLength(String[] values) {
+        return Arrays.stream(values).collect( Collectors.summarizingInt( s -> s.length() ) ).getAverage();
+    }
+
+    @Test
+    void exploreCollectorsSummarising()    {
+        // Given: an array of String
+        String[] colours  = {"Red", "Yellow", "Green", "Blue"};
+
+        // Then: summarising aggregations can be preformed (e.g. sum, min, max)
+        assertEquals(4, countResults(colours));
+        assertEquals(18, totalLength(colours));
+        assertEquals(6, maxLength(colours));
+        assertEquals(3, minLength(colours));
+        assertEquals(4.5, meanLength(colours));
     }
 }
