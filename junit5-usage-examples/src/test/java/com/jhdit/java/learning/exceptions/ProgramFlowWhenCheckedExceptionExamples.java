aaa5778781a1ccc5aaf4bf2a809f178b4c1e7d28
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-08 17:03:21 +0100

    Renamed (for consistency with other Java exploration unit test classes)

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/exceptions/ProgramFlowWhenCheckedExceptionExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/exceptions/ProgramFlowWhenCheckedExceptionExamples.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.fail;
  * Exploration of program flow when checked exception thrown.
  */
 
-public class ProgramFlowWhenCheckedException {
+public class ProgramFlowWhenCheckedExceptionExamples {
 
     @Test
     void exploreProgramWithCheckedExceptionThatMustBeHandledOrDeclaredThrown()   {
--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/exceptions/ProgramFlowWhenRuntimeExceptionExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/exceptions/ProgramFlowWhenRuntimeExceptionExamples.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.fail;
  * Exploration of program flow when runtime exception thrown.
  */
 
-public class ProgramFlowWhenRuntimeException {
+public class ProgramFlowWhenRuntimeExceptionExamples {
 
     @Test
     void exploreProgramFlowCatchingRuntimeException()   {
