64c4ca317329b67f1770d2cefabff24f18be75dc
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-15 14:43:36 +0100

    Examples of rules for static usage with inner classes.

---
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/classes/nested/StaticRestrictionExamples.java
@@ -0,0 +1,52 @@
+package com.jhdit.java.learning.classes.nested;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Inner classes cannot contains static variables or methods, unless they are also static.
+ */
+
+class Outer {
+    private int x = 5;
+
+    protected class InnerProblematic   {
+        // Static declarations are NOT supported (unless inner class is Static!)
+
+        /*
+        public static int x = 10;
+
+        public static int getX() {
+            return x;
+        }
+         */
+    }
+
+    protected static class InnerOk {
+
+        public static int x = 10;
+
+        public static int getX() {
+            return x;
+        }
+    }
+}
+
+public class StaticRestrictionExamples  {
+    @Test
+    void exploreInnerClassUsage()   {
+        // Setup:
+        Outer outer = new Outer();
+
+        // When: instantiated
+        Outer.InnerProblematic innerProblematic = outer.new InnerProblematic();
+        Outer.InnerOk innerOk = new Outer.InnerOk();
+
+        // Expect:
+        assertTrue(innerProblematic instanceof Outer.InnerProblematic);
+        assertTrue(innerOk instanceof Outer.InnerOk);
+        assertEquals(10, innerOk.getX());
+    }
+}
