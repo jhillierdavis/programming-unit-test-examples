d12ccfc8e6346fca897663e913def63a8ff11079
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-16 20:08:15 +0100

    Examples of enumeration initialisation, where enum has static values.

---
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/classes/initialization/EnumWithStaticValuesInitialisationExamples.java
@@ -0,0 +1,30 @@
+package com.jhdit.java.learning.classes.initialization;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+enum MyEnumWithStaticValues {
+    A, B;
+
+    final static int staticValue = 100;
+    static int anotherStaticValue;
+
+    static {
+        // NB: not possible if used in constructor
+        anotherStaticValue = 100;
+    }
+
+    private MyEnumWithStaticValues() {
+        // Compiler error unless staticValue final (since used in constructor)
+        System.out.println("staticValue is " + staticValue);
+    }
+}
+
+public class EnumWithStaticValuesInitialisationExamples {
+    @Test
+    void explore()  {
+        assertEquals(100, MyEnumWithStaticValues.staticValue);
+        assertEquals(100, MyEnumWithStaticValues.anotherStaticValue);
+    }
+}
