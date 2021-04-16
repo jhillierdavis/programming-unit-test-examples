b0050fb96b69842334073e83d8d0b30f71078450
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-16 11:08:03 +0100

    Examples of verbose and implicit interface declaration.

---
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/classes/interfaces/VerboseInterfaceExamples.java
@@ -0,0 +1,41 @@
+package com.jhdit.java.learning.classes.interfaces;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+abstract interface Verbose // abstract is redundant for interfaces (but allowed by compiler)
+{
+    int staticMemberVariable = 10; // member variables are implicitly static!
+
+    abstract void doSomething(); // abstract is redundant for interface methods (but allowed by compiler)
+
+    static int staticMethod() { return staticMemberVariable; }
+
+    default int defaultMethod() {
+        return staticMemberVariable;
+    }
+}
+
+class VerboseImplementation implements Verbose {
+    public void doSomething() {} // Must be public
+}
+
+
+public class VerboseInterfaceExamples {
+
+    @Test
+    void exploreInterfaceStaticValuesAndMethods() {
+        assertEquals(Verbose.staticMemberVariable, Verbose.staticMethod());
+    }
+
+    @Test
+    void exploreInterfaceDefaultMethods() {
+        // Given: an implementation of an interface
+        Verbose v = new VerboseImplementation();
+
+        // Then: default method can be invoked
+        assertEquals(Verbose.staticMemberVariable, v.defaultMethod());
+        assertEquals(v.staticMemberVariable, v.defaultMethod());
+    }
+}
