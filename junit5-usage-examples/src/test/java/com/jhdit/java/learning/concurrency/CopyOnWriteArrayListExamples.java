d397db43171642e069fbf53620dad0c06a019018
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-20 11:31:02 +0100

    Updated to add use of standard ArrayList for comparison with CopyOnWriteArrayList behaviour.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/concurrency/CopyOnWriteArrayListExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/concurrency/CopyOnWriteArrayListExamples.java
@@ -2,10 +2,14 @@ package com.jhdit.java.learning.concurrency;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * For CopyOnWrite collections:
@@ -16,16 +20,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CopyOnWriteArrayListExamples {
 
+    private void populateGivenList(List<Integer> list)    {
+        list.add(4);
+        list.add(2);
+        list.add(8);
+    }
+
     @Test
     void exploreAddWithIterator()   {
         // Given: a populated copy on write list
-        CopyOnWriteArrayList<Integer> cowList = new CopyOnWriteArrayList<>();
-        cowList.add(4);
-        cowList.add(2);
+        List<Integer> cowList = new CopyOnWriteArrayList<>();
+        populateGivenList(cowList);
 
         // When: an iterator is obtained, then the iterator reference remains a snapshot
         Iterator<Integer> it = cowList.iterator();
-        cowList.add(6);
+        cowList.add(7);
 
         // Then: count of iterable items remains 2 (not updated to 3)
         int count = 0;
@@ -34,17 +43,15 @@ public class CopyOnWriteArrayListExamples {
             count++;
         }
         System.out.println();
-        assertEquals(2, count);
-        assertEquals(3, cowList.size());
+        assertEquals(3, count);
+        assertEquals(4, cowList.size());
     }
 
     @Test
     void exploreRemoveWithIterator()    {
         // Given: a populated copy on write list
-        CopyOnWriteArrayList<Integer> cowList = new CopyOnWriteArrayList<>();
-        cowList.add(4);
-        cowList.add(2);
-        cowList.add(6);
+        List<Integer> cowList = new CopyOnWriteArrayList<>();
+        populateGivenList(cowList);
 
         // When: an iterator is obtained, then the iterator reference remains a snapshot
         Iterator<Integer> it = cowList.iterator();
@@ -59,5 +66,40 @@ public class CopyOnWriteArrayListExamples {
         System.out.println();
         assertEquals(3, count);
         assertEquals(2, cowList.size());
+    }
+
+
+    @Test
+    void addWithArrayListForComparisonToCopyOnWrite()    {
+        // Given: a populated copy on write list
+        List<Integer> list = new ArrayList<>();
+        populateGivenList(list);
+
+        // When: an iterator is obtained, then the iterator reference remains a snapshot
+        Iterator<Integer> it = list.iterator();
+        list.add(7);
+
+        assertThrows(ConcurrentModificationException.class, () -> {
+            while (it.hasNext()) {
+                System.out.print(it.next() + " ");
+            }
+        });
+    }
+
+    @Test
+    void removeWithArrayListForComparisonToCopyOnWrite()    {
+        // Given: a populated copy on write list
+        List<Integer> list = new ArrayList<>();
+        populateGivenList(list);
+
+        // When: an iterator is obtained, then the iterator reference remains a snapshot
+        Iterator<Integer> it = list.iterator();
+        list.remove(2);
+
+        assertThrows(ConcurrentModificationException.class, () -> {
+            while (it.hasNext()) {
+                System.out.print(it.next() + " ");
+            }
+        });
     }
 }
