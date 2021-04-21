bc7a7d6d9a2a62976ecd613230fca4bfd0753cdd
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-21 15:52:49 +0100

    Added more examples.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/lambda/statements/LambdaStatementExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/lambda/statements/LambdaStatementExamples.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.function.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LambdaStatementExamples {
 
     @Test
@@ -17,6 +19,27 @@ public class LambdaStatementExamples {
         Supplier<String> hello = () -> "Hello"; // Supplies a result
 
         BiPredicate<Integer, Integer> integerTest = (x,y) -> x == y;
+
+        Consumer<String> conSingle = x -> System.out.println(x);
+        Consumer<String> conSingleWithBrackets = (x) -> System.out.println(x);
+        Consumer<String> conSingleWithParen = (x) -> { System.out.println(x); };
+        Consumer<String> conSingleWithType = (String x) -> System.out.println(x);
+        Consumer<String> conSingleWithTypeAndFinal = (final String x) -> System.out.println(x);
+        Consumer<String> conSingleWithTypeAndFinalAndBrackets = (final String x) -> { System.out.println(x); };
+        Consumer<String> conSingleWithVar = (var x) -> System.out.println(x);
+        Consumer<String> conSingleWithVarAndFinal = (final var x) -> System.out.println(x);
+
+        BiConsumer<String, String> biCon = (final var x, var y) -> System.out.println(x+y);
+
+        Consumer<Integer> lambda = (x) -> {
+            x++;
+            System.out.println(x);
+        };
+
+        Integer integer = Integer.valueOf(0);
+        assertEquals(1, ++integer); // Can increment wrapper classes
+
+
     }
 
     @Test
@@ -31,6 +54,15 @@ public class LambdaStatementExamples {
 
         Function<String> hello = () -> "Hello"; // Wrong number of type arguments: 1; required: 2
 
+        String city = "Munich";
+        BiFunction<Integer, Integer, String> lambaSubstring = String::substring; // Non-static method cannot be referenced from a static context
+
+        Consumer<String> con1 = final String x -> System.out.println(x); // Invalid syntax (requires parenthesis)
+
+        Function<Double, Double> lambdaCircumference = r -> { double circumference = 2 * Math.PI * r; }; // Missing return statement
+
         */
+
+
     }
 }
