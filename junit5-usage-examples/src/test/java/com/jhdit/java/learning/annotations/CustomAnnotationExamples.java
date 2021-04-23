cb6a25cf22f914fdff86884b51d5ea3a8d65f55b
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-04-23 10:14:58 +0100

    Added use of more annotations (e.g. standard: @Deprecated) & verification checks.

--- junit5-usage-examples/src/test/java/com/jhdit/java/learning/annotations/CustomAnnotationExamples.java
+++ junit5-usage-examples/src/test/java/com/jhdit/java/learning/annotations/CustomAnnotationExamples.java
@@ -1,17 +1,24 @@
 package com.jhdit.java.learning.annotations;
 
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // @Target(value = { METHOD }) // Invalid as not applied on a method in this example!
+@Retention(RetentionPolicy.RUNTIME)
 @Target(value = { FIELD, CONSTRUCTOR })
 @interface CustomAnnotation {
     String owner();
     int count() default 100; // Must have a default as not specified
 }
 
-
+@Deprecated
 class Store    {
     public String desc;
 
@@ -22,4 +29,23 @@ class Store    {
 }
 
 public class CustomAnnotationExamples {
+
+    @Test
+    void explore()  {
+        // Given: an object with annotations
+        Store store = new Store("Local store");
+
+        // When: obtain class meta-data
+        Class<? extends Store> clazz = store.getClass();
+
+        // Then: expected class level annotations are present (at runtime)
+        assertTrue(clazz.isAnnotationPresent(Deprecated.class));
+        assertFalse(clazz.isAnnotationPresent(CustomAnnotation.class));
+
+        // And: expected method level annotations are present (at runtime, may require use of RetentionPolicy.RUNTIME!)
+        for (var constructor: clazz.getConstructors())  {
+            // NB: Would have failed without use of RetentionPolicy.RUNTIME
+            assertTrue(constructor.isAnnotationPresent(CustomAnnotation.class));
+        }
+    }
 }
