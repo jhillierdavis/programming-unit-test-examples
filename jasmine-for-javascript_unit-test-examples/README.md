a90102eeec8761fce72c38245ad5e4d88374f4b8
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-11-03 12:49:54 +0000

    Initial Jasmine example implementation

---
+++ jasmine-for-javascript_unit-test-examples/README.md
@@ -0,0 +1,4 @@
+# Jasmine for behaviour-drvien Javacript testing 
+Unit test examples using Jasmine.
+
+See https://jasmine.github.io/pages/getting_started.html
---
+++ jasmine-for-javascript_unit-test-examples/spec/IsJasmineWorking.js
@@ -0,0 +1,5 @@
+describe("Verify that Jasmine is working", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
