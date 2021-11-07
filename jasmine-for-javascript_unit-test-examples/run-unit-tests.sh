f6ad8b743fc21bb4234d39ef449e79d79c000b86
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-11-07 22:22:10 +0000

    Amends to also allow command-line execution via NodeJS

--- jasmine-for-javascript_unit-test-examples/run-unit-tests.sh
+++ jasmine-for-javascript_unit-test-examples/run-unit-tests.sh
@@ -1,5 +1,7 @@
 # Requires NodeJS (e.g. via 'nvm' - node version manager)
 
+# Configured via ./spec/support/jasmine.json (by default) e.g. for location of spec. files
+
 #npx jasmine
 #npx jasmine --config=spec/support/jasmine.json
 npx jasmine-node --verbose --junitreport --noColor spec
