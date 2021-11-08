b67239c88e1f4c68923c5fb5d86afb3cd8384a7c
jhillierdavis <jhillierdavis@users.noreply.github.com>
2021-11-08 16:32:27 +0000

    Removed optional semi-colons

--- jasmine-for-javascript_unit-test-examples/spec/IsJasmineWorkingSpec.js
+++ jasmine-for-javascript_unit-test-examples/spec/IsJasmineWorkingSpec.js
@@ -1,5 +1,7 @@
 describe("Verify that Jasmine is working", function() {
+
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-});
+    expect(true).toBe(true)
+  })
+
+})
