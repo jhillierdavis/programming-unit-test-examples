c724c44d42bc2e8066d2a5539ca87481d83d9a72
jhillierdavis <jhillierdavis@users.noreply.github.com>
2022-02-11 21:25:07 +0000

    Amended to use module.exports

--- jest-for-javascript-unit-test-examples/src/sum.test.js
+++ jest-for-javascript-unit-test-examples/src/sum.test.js
@@ -1,19 +1,19 @@
 // Examples taken from Jest introduction at https://jestjs.io/docs/getting-started
 
-const sum = require('./sum');
+const sum = require('./sum')
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+  expect(sum.add(1, 2)).toBe(3);
+})
 
 test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
-});
+  expect(2 + 2).toBe(4)
+})
 
 test('adding positive numbers is not zero', () => {
   for (let a = 1; a < 10; a++) {
     for (let b = 1; b < 10; b++) {
-      expect(a + b).not.toBe(0);
+      expect(a + b).not.toBe(0)
     }
   }
-});
+})
