8fe21b154f66390b84c82f7225feee97a2872443
jhillierdavis <jhillierdavis@users.noreply.github.com>
2024-01-23 15:47:28 +0000

    Update to cryptographic example using provided password

--- pytest-usage-examples/cryptographic_functions.py
+++ pytest-usage-examples/cryptographic_functions.py
@@ -4,7 +4,10 @@ import hashlib
 def hash_using_sha256(input):
     return hashlib.sha256((input).encode()).hexdigest()
 
+import base64
 from cryptography.fernet import Fernet # pip install cryptography
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 # Fernet us an implementation of symmetiuc authentication cryptography (see https://cryptography.io/en/latest/fernet/ )
 
@@ -12,6 +15,17 @@ ASCII_TEXT = 'ascii'
 
 def generate_encryption_key():
     key = Fernet.generate_key()
+    return key
+
+def generate_encryption_key_using_password(password, salt='salt'):
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt.encode(ASCII_TEXT),
+        iterations=480000,
+    )    
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode(ASCII_TEXT)))
     return key
 
 def encrypt_using_key(input, key):
--- pytest-usage-examples/test_cryptographic_functions.py
+++ pytest-usage-examples/test_cryptographic_functions.py
@@ -1,12 +1,12 @@
 import pytest
 
-from cryptographic_functions import hash_using_sha256, generate_encryption_key, encrypt_using_key, decrypt_using_key
+from cryptographic_functions import hash_using_sha256, generate_encryption_key, encrypt_using_key, decrypt_using_key, generate_encryption_key_using_password
 
 def test_hash_using_sha256():
     assert hash_using_sha256('john_smith@bogus.com') == '0a17a7d96dfaa2302f567b663aac9571fded7fe80d780fe8a0586852c4b7acdc'
 
 
-def test_encrypt_and_decrypt_using_fernet():
+def test_fernet_encrypt_and_decrypt():
     original_message = 'Keep this secret!'
 
     key = generate_encryption_key()
@@ -14,6 +14,28 @@ def test_encrypt_and_decrypt_using_fernet():
     encrypted_message = encrypt_using_key(original_message, key)
     assert original_message != encrypted_message
     
+    decrypted_message = decrypt_using_key(encrypted_message, key)
+    assert encrypted_message != decrypted_message
+    assert original_message == decrypted_message
+
+
+def test_fernet_encrypt_and_decrypt_using_password():
+    # Given: message and password
+    original_message = 'Top secret!'
+    password = 'LetMeIn!'
+
+    # When: a key is generated via the password
+    key = generate_encryption_key_using_password(password)
+
+    # Then: a message can be encrypted
+    encrypted_message = encrypt_using_key(original_message, key)
+    assert original_message != encrypted_message
+
+    # And: Check that key generation works afresh
+    key = None
+    key = generate_encryption_key_using_password(password)
+    
+    # Then: the resulting ciphertext can be decrypted (to obtain the original message)
     decrypted_message = decrypt_using_key(encrypted_message, key)
     assert encrypted_message != decrypted_message
     assert original_message == decrypted_message
